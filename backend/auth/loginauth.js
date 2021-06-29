82b352fc1b2caa76252e1c34dd3c8c7a550dbb5c
rishabh090900 <60057118+rishabh090900@users.noreply.github.com>
2021-06-29 14:22:23 +0530

    Add files via upload

---
+++ backend/auth/loginauth.js
@@ -0,0 +1,40 @@
+
+export const isUserLoggedIn = () => {
+    return async dispatch => {
+        const token = localStorage.getItem('token');
+        if(token){
+            const user = JSON.parse(localStorage.getItem('user'));
+            dispatch({
+                type: authConstants.LOGIN_SUCCESS,
+                payload: {
+                    token, user
+                }
+            });
+        }else{
+            dispatch({
+                type: authConstants.LOGIN_FAILURE,
+                payload: { error: 'Failed to login' }
+            });
+        }
+    }
+}
+
+export const signout = () => {
+    return async dispatch => {
+
+        dispatch({ type: authConstants.LOGOUT_REQUEST });
+        const res = await axios.post(`/admin/signout`);
+
+        if(res.status === 200){
+            localStorage.clear();
+            dispatch({ type: authConstants.LOGOUT_SUCCESS });
+        }else{
+            dispatch({
+                type: authConstants.LOGOUT_FAILURE,
+                payload: { error: res.data.error }
+            });
+        }
+
+        
+    }
+}
